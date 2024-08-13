13d3f8b3afe5cf5782a465a4b08115d3f8919703
Melissa <melissahuertadev@gmail.com>
2024-08-12 23:07:45 -0500

    feat: export

--- VariablesGlobales/Global.gd
+++ VariablesGlobales/Global.gd
@@ -3,5 +3,5 @@ var sprite_clickeado = false
 var sprite_seleccionado = null
 var sprite_anomalianull = null
 var contador_aciertos = 0  
-var dias_por_sobrevivir = 2#4
+var dias_por_sobrevivir = 4
 var player_eligio_descansar = false;
